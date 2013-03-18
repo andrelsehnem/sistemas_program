3e75bc727d538b87e2cdd676f73fd35ddf04d65f
Andre <Andre@Andre-PC>
2013-03-18 20:12:18 -0300

    criado projeto

--- src/br/andre/turmas/Sistema.java
+++ src/br/andre/turmas/Sistema.java
@@ -16,17 +16,13 @@ public class Sistema {
         int x=0;
         Instituição instituicao;
         sistema:while (x==0){
-            System.out.println("1-Gerenciar alunos; 2-Gerenciar turmas; 3-Nova instituição; 4-Informalções; 0-Fim;");
+            System.out.println("1-Gerenciar alunos; 2-Gerenciar turmas; 3-Informalções; 0-Fim;");
             int y = sc.nextInt();
             prim:if (y == 1){
                 System.out.println("1-Matricular Aluno; 2-Cancelar matricula; 3-Voltar");
             }else if(y==2){
                 System.out.println("1-Nova Turma; 2-Remover turma; 3-Voltar");
             }else if(y==3){
-                System.out.println("Digite o nome da instituição: ");
-                String descr = sc.nextLine();
-                instituicao = new Instituição(descr);
-            }else if(y==4){
                 System.out.println("1-Aluno; 2-Turma");
                 y = sc.nextInt();
                 if(y==1){
